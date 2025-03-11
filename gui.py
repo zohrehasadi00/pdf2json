3a78eb235a02af7a0211c24716f7eb916e08ef0e
zohre <zohrehasadi2018@yahoo.com>
2025-03-11 22:35:47 +0100

    added another placeholder: "Save response to"

--- gui.py
+++ gui.py
@@ -1,8 +1,10 @@
-import FreeSimpleGUI as sg
-import json
 import os
-from PIL import Image
 import io
+import FreeSimpleGUI as sg
+from PIL import Image
+from typing import List
+from pathlib import Path
+# import json
 
 
 def resize_image(image_path, target_width, target_height):
@@ -14,17 +16,26 @@ def resize_image(image_path, target_width, target_height):
 
     return bio.getvalue()
 
-def papaias():
+
+def papaias() -> List:
     sg.theme("Green")
-    resized_image = resize_image(r"/statics/fu-logo.png", 500, 170)
+    resized_image = resize_image(r"C:\Users\zohre\bachelorT\MediLink\statics\fu-logo.png", 500, 170)
     layout = [
         [sg.Text("MediLink - Freie Universität, Berlin", justification="center", size=(50, 1), font=("Helvetica", 20))],
-        [sg.Image(data=resized_image, pad=((42,0), (20,20)))],
-        [sg.InputText(key="file_path", enable_events=True, visible=True, size=(57,70), pad=((42,0), (30,30)) ), sg.FileBrowse("Browse", file_types=(("PDF Dateien", "*.pdf"),), key="browse", button_color="Black", size=(10, 2))],
-        [sg.Button("Process", key="start", size=(20, 3), button_color="green", pad=((80, 50), (0,0))), sg.Button("Cancel", key="exit", size=(20, 3), button_color="red")]
+        [sg.Image(data=resized_image, pad=((42, 0), (20, 20)))],
+        [sg.Text("PDF file:", size=(6, 1), font=("Helvetica", 12, "bold")),
+         sg.InputText(key="file_path", enable_events=True, visible=True, size=(57, 70), pad=((0, 0), (30, 30))),
+         sg.FileBrowse("Browse", file_types=(("PDF Dateien", "*.pdf"),), key="browse", button_color="Black",
+                       size=(10, 2))],
+        [sg.Text("Save Results To:", size=(13, 1), font=("Helvetica", 12, "bold")),
+         sg.InputText(key="save_folder", enable_events=True, size=(45, 1), pad=((0, 0), (10, 10))),
+         sg.FolderBrowse("Browse Folder", key="browse_folder", button_color="Black", size=(14, 2))],
+        [sg.Button("Process", key="start", size=(20, 2), button_color="green", pad=((52, 50), (4, 0)),
+                   font=("Helvetica", 12, "bold")),
+         sg.Button("Cancel", key="exit", size=(20, 2), button_color="red", font=("Helvetica", 12, "bold"))]
     ]
 
-    window = sg.Window("PDF Auswahl", layout, size=(600, 450))
+    window = sg.Window("PDF Auswahl", layout, size=(600, 470))
 
     while True:
         event, values = window.read()
@@ -34,9 +45,14 @@ def papaias():
 
         if event == "start":
             pdf_path = values["file_path"]
+            save_to = values["save_folder"]
             if pdf_path and os.path.exists(pdf_path):
-                return pdf_path
+                window.minimize()
+                return [Path(pdf_path), Path(save_to)]
             else:
                 sg.popup("Bitte wählen Sie eine gültige PDF-Datei.", title="Fehler")
 
     window.close()
+
+
+papaias()
